650e3e02042947a284fc1a38b21127377d4d5bf3
alexbhend <alexbhend@cmail.carleton.ca>
2021-01-05 16:47:08 -0500

    Rounded litres calculation to 3 decimals

--- SYSC4907-Alex/WaterFlow.py
+++ SYSC4907-Alex/WaterFlow.py
@@ -21,7 +21,7 @@ def measureFlow(channel):
 IO.add_event_detect(FLOW_SENSOR, IO.FALLING, callback = measureFlow)
 
 while True:
-    AinL = count/450    # AinL is total water usage in litres
+    AinL = round(count/450, 3)   # AinL is total water usage in litres
     print("/n"AinL" L")
     time.sleep(5) # Update every 5 s
 
