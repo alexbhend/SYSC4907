e6e3f28c737759ddd406aabee226b29fa2d54d26
alexbhend <alexbhend@cmail.carleton.ca>
2021-02-05 16:51:41 -0500

    Alex: thingspeak post bug fix

--- SYSC4907-Alex/ThingSpeak.py
+++ SYSC4907-Alex/ThingSpeak.py
@@ -3,17 +3,15 @@ import urllib
 import time
 
 def thingspeak_post(Litres, key):
-
-        params = urllib.urlencode({'field1':Litres, 'key':key }) 
-        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = httplib.HTTPConnection("api.thingspeak.com:80")
-       
-        try:
-            conn.request("POST", "/update", params, headers)
-            response = conn.getresponse()
-            print (response.status, response.reason)
-            data = response.read()
-            print(data)
-            conn.close()
-        except:
-            print ("connection failed")
+    params = urllib.urlencode({'field1':Litres, 'key':key }) 
+    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    try:
+        conn.request("POST", "/update", params, headers)
+        response = conn.getresponse()
+        print (response.status, response.reason)
+        data = response.read()
+        print(data)
+        conn.close()
+    except:
+        print ("connection failed")
