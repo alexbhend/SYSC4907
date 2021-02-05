96e1cd0d5d4d1b98f87cf111ac0e91fa1b4ae89f
alexbhend <alexbhend@cmail.carleton.ca>
2021-02-05 16:15:24 -0500

    Alex: rounding error fix

--- SYSC4907-Alex/WaterFlow.py
+++ SYSC4907-Alex/WaterFlow.py
@@ -23,7 +23,7 @@ def measureFlow(channel):
 IO.add_event_detect(FLOW_SENSOR, IO.FALLING, callback = measureFlow)
 
 while True:
-    AinL = count/450    # AinL is total water usage in litres
+    AinL = round((float(count)/float(450)), 3)    # AinL is total water usage in litres
     print(str(AinL) + " L \n")
     time.sleep(5) # Update every 5 s
 
