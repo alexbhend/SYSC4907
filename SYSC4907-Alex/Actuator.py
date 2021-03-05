aff141fce84e33cce8fad515266c738ef7e9b9a8
alexbhend <alexbhend@cmail.carleton.ca>
2021-03-05 17:34:12 -0500

    Alex: trying with different port

--- SYSC4907-Alex/Actuator.py
+++ SYSC4907-Alex/Actuator.py
@@ -6,18 +6,19 @@ SOLENOID_VALVE = 23 ## Enter pin number here
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
 
-IO.setup(SOLENOID_VALVE, IO.OUT)
+IO.setup(SOLENOID_VALVE, IO.OUT, initial = IO.LOW)
 
 while True:
-        IO.output(SOLENOID_VALVE, True)
+    try:
+        IO.output(SOLENOID_VALVE, IO.HIGH)
         print("Opening valve")
         time.sleep(1)
-        IO.output(SOLENOID_VALVE, False)
+        IO.output(SOLENOID_VALVE, IO.LOW)
         print("Closing valve")
         time.sleep(5)
-"""
+
     except KeyboardInterrupt:
         print("Keyboard interrupt detected")
         IO.cleanup()
         sys.exit()
-"""
+
