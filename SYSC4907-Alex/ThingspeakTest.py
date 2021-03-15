bc9a66d30cfce589c10965ec066ca1ffcd6f9d09
alexbhend <alexbhend@cmail.carleton.ca>
2021-03-14 21:25:28 -0400

    Alex: testing actuateNow function in Actuator.py

--- SYSC4907-Alex/ThingspeakTest.py
+++ SYSC4907-Alex/ThingspeakTest.py
@@ -1,4 +1,13 @@
 from ThingSpeak import thingspeak_post
 import time
 
-thingspeak_post("123", "actuateNow", "", "", "False", "")
+count = 1
+
+while True:
+    if(count%2 == 0):
+        thingspeak_post("123", "actuateNow", "", "", "False", "")
+        count += 1
+    else:
+        thingspeak_post("123", "actuateNow", "", "", "True", "")
+        count += 1
+    time.sleep(10)
