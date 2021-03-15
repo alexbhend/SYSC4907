844cc30a65e611257823cdf6c1590d57f933e0e1
alexbhend <alexbhend@cmail.carleton.ca>
2021-03-14 21:22:23 -0400

    Alex: testing actuateNow function in Actuator.py

--- SYSC4907-Alex/ThingspeakTest.py
+++ SYSC4907-Alex/ThingspeakTest.py
@@ -1,8 +1,4 @@
 from ThingSpeak import thingspeak_post
 import time
 
-thingspeak_post("123", "actuateNow", "", "", "True", "")
-
-time.sleep(11)
-
 thingspeak_post("123", "actuateNow", "", "", "False", "")
