f27ed40189e168e621f26e4f6cdfc0e34bfa999d
alexbhend <alexbhend@cmail.carleton.ca>
2021-04-05 13:47:01 -0400

    Alex: added print statements to test response time

--- SYSC4907-Alex/ThingspeakTest.py
+++ SYSC4907-Alex/ThingspeakTest.py
@@ -3,8 +3,8 @@ import time
 
 
 #thingspeak_post("123", "actuateNow", "", "", "False", "")
-print("Sent start message at: " + str(time.time()))
-thingspeak_post("1234", "actuateNow", "", "", "True", "")
-time.sleep(5)
+#print("Sent start message at: " + str(time.time()))
+#thingspeak_post("1234", "actuateNow", "", "", "True", "")
+#time.sleep(5)
 print("Sent end message at: " + str(time.time()))
 thingspeak_post("1234", "actuateNow", "", "", "False", "")
