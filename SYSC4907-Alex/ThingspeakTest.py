d4ebee34fb504e2b1cbcfac9888da27a30f3feaf
alexbhend <alexbhend@cmail.carleton.ca>
2021-04-05 13:35:27 -0400

    Alex: added print statements to test response time

--- SYSC4907-Alex/ThingspeakTest.py
+++ SYSC4907-Alex/ThingspeakTest.py
@@ -4,4 +4,4 @@ import time
 
 #thingspeak_post("123", "actuateNow", "", "", "False", "")
 print("Sent message at: " + str(time.time()))
-thingspeak_post("1234", "actuateNow", "", "", "True", "")
+thingspeak_post("123", "actuateNow", "", "", "False", "")
