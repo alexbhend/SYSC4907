840134d6e048cbc2a04a7a672d3e0befb6967b94
alexbhend <alexbhend@cmail.carleton.ca>
2021-04-10 15:14:44 -0400

    Alex: changed ThingspeakTest.py to test starting a job after the start job time

--- SYSC4907-Alex/ThingspeakTest2.py
+++ SYSC4907-Alex/ThingspeakTest2.py
@@ -2,10 +2,10 @@ from ThingSpeak import thingspeak_post
 import time
 
 print("Sent newJob1 message at: " + str(time.time()))
-thingspeak_post("1234", "newJob", "", "", "", "16:00:00-18:00:00")
+thingspeak_post("123", "newJob", "", "", "", "16:00:00-18:00:00")
 time.sleep(16)
 print("Sent newJob2 message at: " + str(time.time()))
-thingspeak_post("1234", "newJob", "", "", "", "17:00:00-18:00:00")
+thingspeak_post("123", "newJob", "", "", "", "17:00:00-18:00:00")
 time.sleep(16)
-print("Sent removeJob message at: " + str(time.time()))
-thingspeak_post("1234", "removeJob", "", "", "", "16:00:00-18:00:00")
+print("Sent deleteJob message at: " + str(time.time()))
+thingspeak_post("123", "deleteJob", "", "", "", "16:00:00-18:00:00")
