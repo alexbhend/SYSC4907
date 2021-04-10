2a50bed37d861c8d4f26688e0424007e59c3b1e5
alexbhend <alexbhend@cmail.carleton.ca>
2021-04-10 14:51:25 -0400

    Alex: changed ThingspeakTest.py to test starting a job after the start job time

--- SYSC4907-Alex/ThingspeakTest.py
+++ SYSC4907-Alex/ThingspeakTest.py
@@ -3,4 +3,4 @@ import time
 
 
 print("Sent start message at: " + str(time.time()))
-thingspeak_post("123", "newJob", "", "", "", "14:00-18:00")
+thingspeak_post("123", "newJob", "", "", "", "14:00:00-18:00:00")
