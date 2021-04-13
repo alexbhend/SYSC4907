11523d679763581ed86df8446db3a84e439105fa
alexbhend <alexbhend@cmail.carleton.ca>
2021-04-13 17:42:05 -0400

    Alex: inputted test for demo

--- SYSC4907-Alex/ThingspeakTest2.py
+++ SYSC4907-Alex/ThingspeakTest2.py
@@ -2,7 +2,4 @@ from ThingSpeak import thingspeak_post
 import time
 
 print("Sent newJob1 message at: " + str(time.time()))
-thingspeak_post("123", "newJob", "", "", "", "14:00:00-15:37:00")
-time.sleep(16)
-print("Sent deleteJob message at: " + str(time.time()))
-thingspeak_post("123", "deleteJob", "", "", "", "14:00:00-15:37:00")
+thingspeak_post("111", "newJob", "", "", "", "17:50:00-17:50:30")
